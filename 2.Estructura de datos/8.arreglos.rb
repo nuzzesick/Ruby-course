49af3e1236bc4606fd8766d6f9cfe094b2989b87
Matías <mati280341@gmail.com>
2020-08-06 15:17:11 -0300

    unshift and push explained

--- 2.Estructura de datos/8.arreglos.rb
+++ 2.Estructura de datos/8.arreglos.rb
@@ -5,11 +5,16 @@ puts "Hola, este es el actual arreglo de números: #{numeros}"
 puts "Mostrando en pantalla el primer número en el arreglo: #{numeros.first}"
 # El método last devuelve el último elemento de un arreglo
 puts "Mostrando en pantalla el último número en el arreglo: #{numeros.last}"
-print "Ahora podrás agregar el número que quieras al arreglo: "
+# El método unshift nos permite añadir un elemento nuevo al final del arreglo
+print "Ahora podrás agregar el número que quieras al inicio del arreglo: "
 numero = Integer(gets.chomp)
-# El método push nos permite añadir un elemento nuevo al arreglo
+numeros.unshift(numero)
+puts "¡Has agregado el número '#{numero}' al inicio del arreglo correctamente!"
+# El método push nos permite añadir un elemento nuevo al final del arreglo
+print "Ahora podrás agregar el número que quieras al final del arreglo: "
+numero = Integer(gets.chomp)
 numeros.push(numero)
-puts "¡Has agregado el número '#{numero}' correctamente!"
+puts "¡Has agregado el número '#{numero}' al final del arreglo correctamente!"
 puts "Así ha quedado el arreglo ahora: #{numeros}"
 puts "Si quieres, puedes ingresar un número y buscar un elemento: "
 buscar = Integer(gets.chomp)
